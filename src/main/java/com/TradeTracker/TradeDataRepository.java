b4e4e1e69eccac8546d3e9894d20bd4d526e684c
darrenmunroe94 <darren.munroe@gmail.com>
2023-01-25 23:52:37 -0500

    Update TradeDataRepository.java

--- src/main/java/com/TradeTracker/TradeDataRepository.java
+++ src/main/java/com/TradeTracker/TradeDataRepository.java
@@ -3,12 +3,5 @@ package com.TradeTracker;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TradeDataRepository extends CrudRepository<TradeData, Long>{
-	
-	/*
-	 * To implement custom methods for queries:
-	 * 	-create the method where it will be used
-	 * -prefix with the wanted action (findBy)
-	 * -then append the field name with parenthesis
-	 * EG: findByTickerSymbol() or findByMarketSector()
-	 */
+
 }
